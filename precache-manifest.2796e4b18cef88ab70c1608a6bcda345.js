33e2ac396b587db0e76ae34a6d29bca20df12c22
blueMedium <matrakin1@yahoo.com>
2020-06-07 21:11:03 +1000

    Updates

---
+++ precache-manifest.2796e4b18cef88ab70c1608a6bcda345.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a159934dd8762e4604e04e818b854214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7305f1600ad7b433eb3",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "20d0ab49012f4618e882",
+    "url": "/static/css/main.001174ab.chunk.css"
+  },
+  {
+    "revision": "e7305f1600ad7b433eb3",
+    "url": "/static/js/2.29a284ef.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.29a284ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d0ab49012f4618e882",
+    "url": "/static/js/main.efaf73cc.chunk.js"
+  },
+  {
+    "revision": "b83cbc8f1b0516bb5a45",
+    "url": "/static/js/runtime-main.39d042f1.js"
+  }
+]);
